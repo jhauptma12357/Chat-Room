d3e611dda29453bdfebb2abce3659680a61c0183
allarkvarkk <84292598+allarkvarkk@users.noreply.github.com>
2024-05-27 11:08:12 -0400

    Add files via upload

---
+++ Network Testing/src/Client.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException{
+        Socket s = new Socket("localhost", 4999);
+    }
+}
---
+++ Network Testing/src/Server.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException{
+        ServerSocket ss = new ServerSocket(4999);
+        Socket s = ss.accept();
+
+        System.out.println("Client connected");
+    }
+}

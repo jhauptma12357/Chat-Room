a581956756bd35e43020713720a817d4934b2bc1
allarkvarkk <84292598+allarkvarkk@users.noreply.github.com>
2024-05-27 11:08:46 -0400

    Delete Network Testing directory

--- Network Testing/src/Client.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-import java.net.Socket;
-
-public class Client {
-    public static void main(String[] args) throws IOException{
-        Socket s = new Socket("localhost", 4999);
-    }
-}
